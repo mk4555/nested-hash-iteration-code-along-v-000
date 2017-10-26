c29e4822bf84d3be85976baf68eed7725a605106
Min Kim <mk4555@nyu.edu>
2017-10-26 18:49:08 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +13,14 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
+
 def remove_strawberry(contacts)
+  contacts.each do |name, values|
+    values.each do |value, data|
+      if value == :favorite_ice_cream_flavors
+        data.delete_if{|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
 
 end
